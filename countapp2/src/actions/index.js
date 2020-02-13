12e93026509d246a6f2e34142cd04c151320aedf
kuriharahirokazu <hirokazu.kurihara@topgate.co.jp>
2020-02-13 11:08:21 +0900

    init

---
+++ countapp2/src/actions/index.js
@@ -0,0 +1,13 @@
+//Action定義
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+
+//Action Creater(Action Createrを呼び出すことで、stateの更新が行われる)
+export const increment = () =>({
+    type : INCREMENT
+});
+
+export const decrement = () =>({
+    type : DECREMENT
+});
+
